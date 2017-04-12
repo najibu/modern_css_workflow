ef226ee7ebff0c101beabf6181a91ebffbc7aebe
najibu <ns.najibu@gmail.com>
2017-04-12 16:41:58 +0100

    Added components

---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body class="@yield('body_class')">
+
+  @yield('section')
+
+  @yield('footer')
+
+</body>
+</html>
